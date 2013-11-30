25cc0894b16c56dca081ad95f9fa7563d76f8691
ivan <ivan@IVCASTRO-LAP.st-users.us.oracle.com>
2013-11-29 19:24:44 -0600

    Starting point of Web Services

---
+++ EvensussWebServices/src/com/evensusss/web/services/EvensussWebServicesServlet.java
@@ -0,0 +1,13 @@
+package com.evensusss.web.services;
+
+import java.io.IOException;
+import javax.servlet.http.*;
+
+@SuppressWarnings("serial")
+public class EvensussWebServicesServlet extends HttpServlet {
+	public void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws IOException {
+		resp.setContentType("text/plain");
+		resp.getWriter().println("Hello, world");
+	}
+}
